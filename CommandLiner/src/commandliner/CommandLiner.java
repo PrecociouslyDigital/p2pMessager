969f1cf26b288c0c785c2f11ca5b058e0c6c411f
precociouslyDigital <fellfromatree@gmail.com>
2015-01-12 09:14:24 -0800

    First!

---
+++ CommandLiner/src/commandliner/CommandLiner.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package commandliner;
+
+/**
+ *
+ * @author s-yinb
+ */
+public class CommandLiner {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Runtime rt = Runtime.getRuntime();
+        Scanner scn = new Scanner(System.in);
+        String input = scn.nextLine();
+        while(input.equalsIgnoreCase)
+    }
+    
+}
